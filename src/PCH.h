fe8065885b3d15e97f9b984209389673c409c8fb
VersuchDrei <FalkoMitC@gmail.com>
2022-10-11 09:05:43 +0200

    migrated so clib-ng

---
+++ src/PCH.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+#include <REL/Relocation.h>
+#include <SKSE/API.h>
+
+#undef cdecl // Workaround for Clang 14 CMake configure error.
+
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/msvc_sink.h>
+
+// Compatible declarations with other sample projects.
+#define DLLEXPORT __declspec(dllexport)
+
+using namespace std::literals;
+using namespace REL::literals;
+
+namespace logger = SKSE::log;
+
+namespace util {
+    using SKSE::stl::report_and_fail;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,114 +1,92 @@
-#include "version.h"
+#include <stddef.h>
 
-#include "RE/Skyrim.h"
-#include "REL/Relocation.h"
-#include "SKSE/API.h"
+using namespace RE::BSScript;
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace SKSE::stl;
 
+namespace {
+    void InitializeLogging() {
+        auto path = log_directory();
+        if (!path) {
+            report_and_fail("Unable to lookup SKSE logs directory.");
+        }
+        *path /= PluginDeclaration::GetSingleton()->GetName();
+        *path += L".log";
 
-namespace
-{
-	class JournalMenuEx : public RE::JournalMenu
-	{
-	public:
-		enum class Tab : UInt32
-		{
-			kQuest,
-			kPlayerInfo,
-			kSystem
-		};
+        std::shared_ptr<spdlog::logger> log;
+        if (IsDebuggerPresent()) {
+            log = std::make_shared<spdlog::logger>(
+                "Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
+        } else {
+            log = std::make_shared<spdlog::logger>(
+                "Global", std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true));
+        }
+        log->set_level(spdlog::level::info);
+        log->flush_on(spdlog::level::info);
 
+        spdlog::set_default_logger(std::move(log));
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
+    }
 
-		void Hook_Accept(RE::FxDelegateHandler::CallbackProcessor* a_cbReg)	// 01
-		{
-			_Accept(this, a_cbReg);
-			fxDelegate->callbacks.Remove("RememberCurrentTabIndex");
-			a_cbReg->Process("RememberCurrentTabIndex", RememberCurrentTabIndex);
-		}
+    class JournalMenuEx : public RE::JournalMenu {
+    public:
+        enum class Tab : std::uint32_t { kQuest, kPlayerInfo, kSystem };
 
+        void Hook_Accept(RE::FxDelegateHandler::CallbackProcessor* a_cbReg)  // 01
+        {
+            _Accept(this, a_cbReg);
+            fxDelegate->callbacks.Remove("RememberCurrentTabIndex");
+            a_cbReg->Process("RememberCurrentTabIndex", RememberCurrentTabIndex);
+        }
 
-		RE::UI_MESSAGE_RESULTS Hook_ProcessMessage(RE::UIMessage& a_message)	// 04
-		{
-			using Message = RE::UI_MESSAGE_TYPE;
-			if (a_message.type == Message::kShow) {
-				auto ui = RE::UI::GetSingleton();
-				auto uiStr = RE::InterfaceStrings::GetSingleton();
-				if (ui->IsMenuOpen(uiStr->mapMenu)) {
-					*_savedTabIdx = Tab::kQuest;
-				} else {
-					*_savedTabIdx = Tab::kSystem;
-				}
-			}
+        RE::UI_MESSAGE_RESULTS Hook_ProcessMessage(RE::UIMessage& a_message)  // 04
+        {
+            using Message = RE::UI_MESSAGE_TYPE;
+            if (a_message.type == Message::kShow) {
+                auto ui = RE::UI::GetSingleton();
+                auto uiStr = RE::InterfaceStrings::GetSingleton();
+                if (ui->IsMenuOpen(uiStr->mapMenu)) {
+                    *_savedTabIdx = Tab::kQuest;
+                } else {
+                    *_savedTabIdx = Tab::kSystem;
+                }
+            }
 
-			return _ProcessMessage(this, a_message);
-		}
+            return _ProcessMessage(this, a_message);
+        }
 
+        static void RememberCurrentTabIndex([[maybe_unused]] const RE::FxDelegateArgs& a_params) { return; }
 
-		static void RememberCurrentTabIndex([[maybe_unused]] const RE::FxDelegateArgs& a_params)
-		{
-			return;
-		}
+        static void InstallHooks() {
+            REL::Relocation<std::uintptr_t> vTable(RE::VTABLE_JournalMenu[0]);
+            _Accept = vTable.write_vfunc(0x1, &JournalMenuEx::Hook_Accept);
+            _ProcessMessage = vTable.write_vfunc(0x4, &JournalMenuEx::Hook_ProcessMessage);
 
+            log::info("Installed hooks");
+        }
 
-		static void InstallHooks()
-		{
-			REL::Offset<std::uintptr_t> vTable(RE::Offset::JournalMenu::Vtbl);
-			_Accept = vTable.WriteVFunc(0x1, &JournalMenuEx::Hook_Accept);
-			_ProcessMessage = vTable.WriteVFunc(0x4, &JournalMenuEx::Hook_ProcessMessage);
+        using Accept_t = decltype(&RE::JournalMenu::Accept);
+        using ProcessMessage_t = decltype(&RE::JournalMenu::ProcessMessage);
 
-			_MESSAGE("Installed hooks");
-		}
-
-
-		using Accept_t = decltype(&RE::JournalMenu::Accept);
-		using ProcessMessage_t = decltype(&RE::JournalMenu::ProcessMessage);
-
-		static inline REL::Function<Accept_t> _Accept;
-		static inline REL::Function<ProcessMessage_t> _ProcessMessage;
-		static inline REL::Offset<Tab*> _savedTabIdx = REL::ID(520167);
-	};
+        static inline REL::Relocation<Accept_t> _Accept;
+        static inline REL::Relocation<ProcessMessage_t> _ProcessMessage;
+        static inline REL::Relocation<Tab*> _savedTabIdx{RELOCATION_ID(520167, 406697)};
+    };
 }
 
+SKSEPluginLoad(const LoadInterface* skse) {
+    InitializeLogging();
 
-extern "C"
-{
-	bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
-	{
-		SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\StayAtSystemPage.log");
-		SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::UseLogStamp(true);
-
-		_MESSAGE("StayAtSystemPage v%s", SYSP_VERSION_VERSTRING);
-
-		a_info->infoVersion = SKSE::PluginInfo::kVersion;
-		a_info->name = "StayAtSystemPage";
-		a_info->version = SYSP_VERSION_MAJOR;
-
-		if (a_skse->IsEditor()) {
-			_FATALERROR("Loaded in editor, marking as incompatible!");
-			return false;
-		}
-
-		auto ver = a_skse->RuntimeVersion();
-		if (ver <= SKSE::RUNTIME_1_5_39) {
-			_FATALERROR("Unsupported runtime version %s!", ver.GetString().c_str());
-			return false;
-		}
-
-		return true;
-	}
+    auto* plugin = PluginDeclaration::GetSingleton();
+    auto version = plugin->GetVersion();
+    log::info("{} {} is loading...", plugin->GetName(), version);
 
 
-	bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-	{
-		_MESSAGE("StayAtSystemPage loaded");
+    Init(skse);
 
-		if (!SKSE::Init(a_skse)) {
-			return false;
-		}
+    JournalMenuEx::InstallHooks();
 
-		JournalMenuEx::InstallHooks();
-
-		return true;
-	}
+    log::info("{} has finished loading.", plugin->GetName());
+    return true;
 }
